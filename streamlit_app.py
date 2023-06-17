675c4280072a1ebe9e3cf5bcbefbe2cf7609ddc6
Mick Fallon <michael.fallon@strataim.co.uk>
2023-06-17 17:47:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')

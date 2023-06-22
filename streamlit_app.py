20b90dc9b93556e6ed4c7320d5f077b406a81792
Mick Fallon <michael.fallon@strataim.co.uk>
2023-06-22 20:57:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,3 +64,9 @@ streamlit.header("The fruit load list contains :")
 #streamlit.dataframe(my_data_row)
 streamlit.dataframe(my_data_rows)
 
+#Allow the user  to add fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?,'Kiwi')
+streamlit.write('The user entered ', add_my_fruit)
+import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+

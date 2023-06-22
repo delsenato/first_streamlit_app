a2f44c3d587063ba473acae8c0195d0ce9781ac9
Mick Fallon <michael.fallon@strataim.co.uk>
2023-06-22 15:32:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
+# This code creates a formatted table with headings.
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)

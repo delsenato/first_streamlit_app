586d568d868907582a53757f6392c98870e08630
Mick Fallon <michael.fallon@strataim.co.uk>
2023-06-22 20:33:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
+
 # created the main python file
 
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favourites')

501bc0a221d4a3d94f1a635a3eb5dd0e2422616c
Mick Fallon <michael.fallon@strataim.co.uk>
2023-06-23 17:09:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,26 +74,33 @@ except URLError as e:
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-#my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
+
+
 streamlit.header("The fruit load list contains :")
-#streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
+#Snowflake related functions
+def get_fruit_load_list():
+        with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.header("The fruit load list contains :")
+    streamlit.dataframe(my_data_rows)
 
 #Allow the user  to add fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('The user entered ', add_my_fruit)
-import requests
-my_cur1 = my_cnx.cursor()
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+#streamlit.write('The user entered ', add_my_fruit)
+#import requests
+#my_cur1 = my_cnx.cursor()
 #my_cur.execute("SELECT * from fruit_load_list")
 #my_cur1.execute("insert into PUBLIC.FRUIT_LOAD_LIST values ('" + add_my_fruit  "')")
 #my_cur1.execute("insert into PUBLIC.FRUIT_LOAD_LIST values ('parrot')")
-my_cur1.execute("insert into PUBLIC.FRUIT_LOAD_LIST values" + f"('{add_my_fruit}')") 
-my_cur3 = my_cnx.cursor()
-my_cur3.execute("SELECT * from fruit_load_list")
+#my_cur1.execute("insert into PUBLIC.FRUIT_LOAD_LIST values" + f"('{add_my_fruit}')") 
+#my_cur3 = my_cnx.cursor()
+#my_cur3.execute("SELECT * from fruit_load_list")
 
 
 #insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST
